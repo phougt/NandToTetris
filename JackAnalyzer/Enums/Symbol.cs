79cc7cd08ade7d306b44e42e7ec76c1c84ddd5e3
Mearaphoumint Mon <74492753+phougt@users.noreply.github.com>
2025-03-19 21:51:50 +0700

    Add Enum 'Symbol'

---
+++ JackAnalyzer/Enums/Symbol.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JackAnalyzer.Enums
+{
+    public enum Symbol
+    {
+        LBRACE,   // {
+        RBRACE,   // }
+        LPAR,     // (
+        RPAR,     // )
+        LBRACK,   // [
+        RBRACK,   // ]
+        DOT,      // .
+        COMMA,    // ,
+        SEMICOLON,// ;
+        PLUS,     // +
+        MINUS,    // -
+        STAR,     // *
+        SLASH,    // /
+        AMP,      // &
+        PIPE,     // |
+        LT,       // <
+        GT,       // >
+        EQUAL,    // =
+        TILDE     // ~
+    }
+}
