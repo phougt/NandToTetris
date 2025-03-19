7f70b42989ed3a69864cde31ae002d4f5b83ceee
Mearaphoumint Mon <74492753+phougt@users.noreply.github.com>
2025-03-19 21:57:41 +0700

    Add ToSymbol() Extension Method For Type 'string'

---
+++ JackAnalyzer/Extensions/StringExtension.cs
@@ -0,0 +1,33 @@
+using JackAnalyzer.Enums;
+namespace JackAnalyzer.Extensions 
+{
+    public static class StringExtension
+    {
+        public static Symbol ToSymbol(this string value)
+        {
+            return value switch
+            {
+                "{" => Symbol.LBRACE,
+                "}" => Symbol.RBRACE,
+                "(" => Symbol.LPAR,
+                ")" => Symbol.RPAR,
+                "[" => Symbol.LBRACK,
+                "]" => Symbol.RBRACK,
+                "." => Symbol.DOT,
+                "," => Symbol.COMMA,
+                ";" => Symbol.SEMICOLON,
+                "+" => Symbol.PLUS,
+                "-" => Symbol.MINUS,
+                "*" => Symbol.STAR,
+                "/" => Symbol.SLASH,
+                "&" => Symbol.AMP,
+                "|" => Symbol.PIPE,
+                "<" => Symbol.LT,
+                ">" => Symbol.GT,
+                "=" => Symbol.EQUAL,
+                "~" => Symbol.TILDE,
+                _ => throw new ArgumentException("Invalid Argument")
+            };
+        }
+    }
+}
