7a6f22329a4704671ddbce38c368e7ec4f5aefeb
Mearaphoumint Mon <74492753+phougt@users.noreply.github.com>
2025-03-22 18:24:22 +0700

    Update CompilationEngine.cs Logic

--- JackAnalyzer/Modules/CompilationEngine.cs
+++ JackAnalyzer/Modules/CompilationEngine.cs
@@ -20,47 +20,98 @@ namespace JackAnalyzer.Modules
         public CompilationEngine(JackTokenizer tokenizer)
         {
             _tokenizer = tokenizer;
+            _output = new StringBuilder();
         }
 
         public void CompileClass()
         {
             _currentResult = _tokenizer.Advance()
-                                    .PrintErrorAndExitIfFailed()
-                                    .ExpectOrExit(Keyword.CLASS);
+                            .PrintErrorAndExitIfFailed()
+                            .ExpectOrExit(Keyword.CLASS);
+
+            _output.AppendLine("<class>");
+            AppendKeyword(_currentResult);
 
             _currentResult = _tokenizer.Advance()
                     .PrintErrorAndExitIfFailed()
                     .ExpectOrExit(TokenType.IDENTIFIER);
 
+            AppendIdentifier(_currentResult);
+
             _currentResult = _tokenizer.Advance()
                     .PrintErrorAndExitIfFailed()
                     .ExpectOrExit(Symbol.LBRACE);
 
+            AppendSymbol(_currentResult);
+
             _currentResult = _tokenizer.Advance()
                             .PrintErrorAndExitIfFailed();
 
-            CompileClassVar();
-            
+            while (_currentResult.Expect(Keyword.STATIC)
+                    || _currentResult.Expect(Keyword.FIELD)
+                    || _currentResult.Expect(Keyword.CONSTRUCTOR)
+                    || _currentResult.Expect(Keyword.FUNCTION)
+                    || _currentResult.Expect(Keyword.METHOD))
+            {
+                if (_currentResult.Expect(Keyword.STATIC) || _currentResult.Expect(Keyword.FIELD))
+                {
+                    CompileClassVar();
+                }
+                else
+                {
+                    CompileSubroutine();
+                }
+            }
+
             _currentResult.PrintErrorAndExitIfFailed()
                         .ExpectOrExit(Symbol.RBRACE);
+
+            AppendSymbol(_currentResult);
+
+            _output.AppendLine("</class>");
+            StreamWriter writer = new StreamWriter("C:\\Users\\phoug\\Desktop\\output.xml");
+            writer.Write(_output.ToString());
+            writer.Close();
         }
 
         private void CompileClassVar()
         {
             if (_currentResult.Expect(Keyword.STATIC) || _currentResult.Expect(Keyword.FIELD))
             {
+                _output.AppendLine("<classVarDec>");
+                AppendKeyword(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed()
+                                .ExpectTypeOrExit();
+
+                if (_currentResult.ExpectBuiltInType())
+                {
+                    AppendKeyword(_currentResult);
+                }
+                else
+                {
+                    AppendIdentifier(_currentResult);
+                }
+
                 _currentResult = _tokenizer.Advance()
                                 .PrintErrorAndExitIfFailed()
                                 .ExpectOrExit(TokenType.IDENTIFIER);
+
+                AppendIdentifier(_currentResult);
 
                 _currentResult = _tokenizer.Advance()
                                 .PrintErrorAndExitIfFailed();
 
                 while (_currentResult.Expect(Symbol.COMMA))
                 {
+                    AppendSymbol(_currentResult);
+
                     _currentResult = _tokenizer.Advance()
                                     .PrintErrorAndExitIfFailed()
                                     .ExpectOrExit(TokenType.IDENTIFIER);
+
+                    AppendIdentifier(_currentResult);
 
                     _currentResult = _tokenizer.Advance()
                                     .PrintErrorAndExitIfFailed();
@@ -68,27 +119,210 @@ namespace JackAnalyzer.Modules
 
                 _currentResult.PrintErrorAndExitIfFailed()
                             .ExpectOrExit(Symbol.SEMICOLON);
-                
+
+                AppendSymbol(_currentResult);
+
                 _currentResult = _tokenizer.Advance()
                                 .PrintErrorAndExitIfFailed();
 
-                CompileClassVar();
+                _output.AppendLine("</classVarDec>");
             }
-
-            CompileSubroutine();
-
         }
 
         private void CompileSubroutine()
         {
+            if (_currentResult.Expect(Keyword.CONSTRUCTOR) || _currentResult.Expect(Keyword.FUNCTION) || _currentResult.Expect(Keyword.METHOD))
+            {
+                _output.AppendLine("<subroutineDec>");
+                AppendKeyword(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed();
+
+                if (!(_currentResult.Expect(Keyword.VOID) || _currentResult.ExpectType()))
+                {
+                    Console.Error.WriteLine("Expected 'void' or type");
+                    Environment.Exit(1);
+                }
+
+                if (_currentResult.Expect(Keyword.VOID) || _currentResult.ExpectBuiltInType())
+                {
+                    AppendKeyword(_currentResult);
+                }
+                else
+                {
+                    AppendIdentifier(_currentResult);
+                }
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed()
+                                .ExpectOrExit(TokenType.IDENTIFIER);
+
+                AppendIdentifier(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed()
+                                .ExpectOrExit(Symbol.LPAR);
+
+                AppendSymbol(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed();
+
+                CompileParameterList();
+
+                _currentResult.PrintErrorAndExitIfFailed()
+                            .ExpectOrExit(Symbol.RPAR);
+
+                AppendSymbol(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed()
+                                .ExpectOrExit(Symbol.LBRACE);
+
+                AppendSymbol(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed();
+
+                while (_currentResult.Expect(Keyword.VAR)
+                        || _currentResult.Expect(Keyword.DO))
+                {
+                    if (_currentResult.Expect(Keyword.VAR))
+                    {
+                        CompileVarDec();
+                    }
+                    else if (_currentResult.Expect(Keyword.DO))
+                    {
+                        CompileDo();
+                    }
+                    else
+                    {
+                        _currentResult = _tokenizer.Advance()
+                                        .PrintErrorAndExitIfFailed();
+                    }
+                }
+
+                _currentResult.PrintErrorAndExitIfFailed()
+                            .ExpectOrExit(Symbol.RBRACE);
+
+                AppendSymbol(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed();
+
+                _output.AppendLine("</subroutineDec>");
+            }
         }
 
         private void CompileParameterList()
         {
+            if (_currentResult.ExpectType())
+            {
+                _output.AppendLine("<parameterList>");
+
+                if (_currentResult.ExpectBuiltInType())
+                {
+                    AppendKeyword(_currentResult);
+                }
+                else
+                {
+                    AppendIdentifier(_currentResult);
+                }
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed()
+                                .ExpectOrExit(TokenType.IDENTIFIER);
+
+                AppendIdentifier(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed();
+
+                while (_currentResult.Expect(Symbol.COMMA))
+                {
+                    AppendSymbol(_currentResult);
+
+                    _currentResult = _tokenizer.Advance()
+                                    .PrintErrorAndExitIfFailed()
+                                    .ExpectTypeOrExit();
+
+                    if (_currentResult.ExpectBuiltInType())
+                    {
+                        AppendKeyword(_currentResult);
+                    }
+                    else
+                    {
+                        AppendIdentifier(_currentResult);
+                    }
+
+                    _currentResult = _tokenizer.Advance()
+                                    .PrintErrorAndExitIfFailed()
+                                    .ExpectOrExit(TokenType.IDENTIFIER);
+
+                    AppendIdentifier(_currentResult);
+
+                    _currentResult = _tokenizer.Advance()
+                                    .PrintErrorAndExitIfFailed();
+                }
+
+                _output.AppendLine("</parameterList>");
+            }
         }
 
         private void CompileVarDec()
         {
+            if (_currentResult.Expect(Keyword.VAR))
+            {
+                _output.AppendLine("<varDec>");
+                AppendKeyword(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                            .PrintErrorAndExitIfFailed()
+                            .ExpectTypeOrExit();
+
+                if (_currentResult.ExpectBuiltInType())
+                {
+                    AppendKeyword(_currentResult);
+                }
+                else
+                {
+                    AppendIdentifier(_currentResult);
+                }
+
+                _currentResult = _tokenizer.Advance()
+                            .PrintErrorAndExitIfFailed()
+                            .ExpectOrExit(TokenType.IDENTIFIER);
+
+                AppendIdentifier(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed();
+
+                while (_currentResult.Expect(Symbol.COMMA))
+                {
+                    AppendSymbol(_currentResult);
+
+                    _currentResult = _tokenizer.Advance()
+                                    .PrintErrorAndExitIfFailed()
+                                    .ExpectOrExit(TokenType.IDENTIFIER);
+
+                    AppendIdentifier(_currentResult);
+
+                    _currentResult = _tokenizer.Advance()
+                                    .PrintErrorAndExitIfFailed();
+                }
+
+                _currentResult.PrintErrorAndExitIfFailed()
+                            .ExpectOrExit(Symbol.SEMICOLON);
+
+                AppendSymbol(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed();
+
+                _output.AppendLine("</varDec>");
+            }
         }
 
         private void CompileStatements()
@@ -97,6 +331,71 @@ namespace JackAnalyzer.Modules
 
         private void CompileDo()
         {
+            if (_currentResult.Expect(Keyword.DO))
+            {
+                _output.AppendLine("<doStatement>");
+                AppendKeyword(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed()
+                                .ExpectOrExit(TokenType.IDENTIFIER);
+
+                AppendIdentifier(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed();
+
+                if (_currentResult.Expect(Symbol.LPAR))
+                {
+                    AppendSymbol(_currentResult);
+
+                    _currentResult = _tokenizer.Advance()
+                                    .PrintErrorAndExitIfFailed();
+                    // CompileExpressionList();
+
+                    _currentResult.PrintErrorAndExitIfFailed()
+                                .ExpectOrExit(Symbol.RPAR);
+
+                    AppendSymbol(_currentResult);
+                }
+                else if (_currentResult.Expect(Symbol.DOT))
+                {
+                    AppendSymbol(_currentResult);
+
+                    _currentResult = _tokenizer.Advance()
+                                    .PrintErrorAndExitIfFailed()
+                                    .ExpectOrExit(TokenType.IDENTIFIER);
+
+                    AppendIdentifier(_currentResult);
+
+                    _currentResult = _tokenizer.Advance()
+                                    .PrintErrorAndExitIfFailed()
+                                    .ExpectOrExit(Symbol.LPAR);
+
+                    AppendSymbol(_currentResult);
+
+                    _currentResult = _tokenizer.Advance()
+                                    .PrintErrorAndExitIfFailed();
+
+                    // CompileExpressionList();
+
+                    _currentResult.PrintErrorAndExitIfFailed()
+                                .ExpectOrExit(Symbol.RPAR);
+                    
+                    AppendSymbol(_currentResult);
+                }
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed()
+                                .ExpectOrExit(Symbol.SEMICOLON);
+
+                AppendSymbol(_currentResult);
+
+                _currentResult = _tokenizer.Advance()
+                                .PrintErrorAndExitIfFailed();
+
+                _output.AppendLine("</doStatement>");
+            }
         }
 
         private void CompileLet()
@@ -125,6 +424,23 @@ namespace JackAnalyzer.Modules
 
         private void CompileExpressionList()
         {
+            _currentResult = _tokenizer.Advance()
+                            .PrintErrorAndExitIfFailed();
+        }
+
+        private void AppendKeyword(Result<Token> token)
+        {
+            _output.AppendLine($"<keyword> {((Keyword)token.Value.Value).ToString().ToLower()} </keyword>");
+        }
+
+        private void AppendIdentifier(Result<Token> token)
+        {
+            _output.AppendLine($"<identifier> {(string)_currentResult.Value.Value} </identifier>");
+        }
+
+        private void AppendSymbol(Result<Token> token)
+        {
+            _output.AppendLine($"<symbol> {((Symbol)_currentResult.Value.Value).ToSymbolString()} </symbol>");
         }
     }
 }
