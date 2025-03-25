79cc965b23026d68f01f3a861263639aae675ff9
Mearaphoumint Mon <74492753+phougt@users.noreply.github.com>
2025-03-25 12:00:13 +0700

    Add Single File, Multiple Files Compilation For JackAnalyzer

--- JackAnalyzer/Program.cs
+++ JackAnalyzer/Program.cs
@@ -12,17 +12,71 @@ namespace JackAnalyzer
     {
         static void Main(string[] args)
         {
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-            string filePath = "C:\\Users\\phoug\\Desktop\\SquareGame.jack";
-            string outputPath = "C:\\Users\\phoug\\Desktop\\SquareGameOut.xml";
+            string firstArg = string.Empty;
+            string secondArg = string.Empty;
 
-            JackTokenizer tokenizer = new JackTokenizer(filePath);
-            CompilationEngine engine = new CompilationEngine(tokenizer, outputPath);
-            engine.CompileClass();
-            engine.TryWriteOutputToFile();
+            bool isMultiFilesTranslating = false;
 
-            Console.WriteLine(watch.Elapsed);
+            if (args.Length == 1)
+            {
+                isMultiFilesTranslating = args[0].Equals("-dir");
+                firstArg = args[0];
+            }
+            else if (args.Length == 2)
+            {
+                firstArg = args[0];
+                secondArg = args[1];
+            }
+            else
+            {
+                Console.Error.WriteLine("Arguments Format is expected to be (SourcePath, OutputPath) or (-dir).");
+                Environment.Exit(1);
+            }
+
+            if (!isMultiFilesTranslating)
+            {
+                JackTokenizer tokenizer = new JackTokenizer(firstArg);
+                CompilationEngine engine = new CompilationEngine(tokenizer, secondArg);
+                engine.CompileClass();
+                if (engine.TryWriteOutputToFile())
+                {
+                    Console.WriteLine($"Compilation for '{firstArg}' is successful.");
+                    Environment.Exit(1);
+                }
+                else
+                {
+                    Console.Error.WriteLine($"Compilation for '{firstArg}' is failed.");
+                    Environment.Exit(1);
+                }
+            }
+            else
+            {
+                try
+                {
+                    string[] files = Directory.GetFiles(Environment.CurrentDirectory, "*.jack");
+                    foreach (string file in files)
+                    {
+                        JackTokenizer tokenizer = new JackTokenizer(file);
+                        CompilationEngine engine = new CompilationEngine(tokenizer, file.Replace(".jack", ".xml"));
+                        engine.CompileClass();
+
+                        if (engine.TryWriteOutputToFile())
+                        {
+                            Console.WriteLine($"Compilation for '{file}' is successful.");
+                        }
+                        else
+                        {
+                            Console.Error.WriteLine($"Compilation for '{file}' is failed.");
+                            Environment.Exit(1);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Error.WriteLine("Compilation is failed.");
+                    Environment.Exit(1);
+                }
+            }
         }
     }
 }
